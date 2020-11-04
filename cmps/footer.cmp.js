c57cca52b9828224a448b919ec8206c6d9fab3d3
Bulla Lulla <bulla@YBKof.local>
2020-11-04 21:11:17 +0200

    css!

--- cmps/footer.cmp.js
+++ cmps/footer.cmp.js
@@ -2,6 +2,7 @@
 export default {
     template: `
     <footer>
+        <small>coffee rights 2020 <i class="fas fa-mug-hot"></i></small>
     </footer>
     `
 }
--- cmps/header.cmp.js
+++ cmps/header.cmp.js
@@ -1,13 +1,21 @@
 
 export default {
     template: `
-    <header>
-        <nav>
-    <router-link to="/" exact>Home page</router-link>
-    <router-link to="/mail" exact>Mail</router-link>
-    <router-link to="/keep" exact>Keep</router-link>
-    <router-link to="/book" exact>Books</router-link>
+    <header class="flex space-between">
+        <div class="logo" @click="goHome">
+            <h1>Appsus <span><i class="fas fa-horse"></i></span></h1>
+        </div>
+        <nav class="flex align-center space-between">
+            <router-link to="/mail" exact>Mail</router-link>
+            <router-link to="/keep" exact>Keep</router-link>
+            <router-link to="/book" exact>Books</router-link>
         </nav>
     </header>
-    `
+    `,
+	
+	methods: {
+		goHome() {
+            this.$router.push(`/`);
+        }
+	}
 }

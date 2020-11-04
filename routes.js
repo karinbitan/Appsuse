89bafb6ee3bc24610d50cdc463a9e94cf0a165d8
Bulla Lulla <bulla@YBKof.local>
2020-11-04 16:33:55 +0200

    conflicts resolved

--- routes.js
+++ routes.js
@@ -1,11 +1,6 @@
 import homePage from './pages/home-page.cmp.js';
-<<<<<<< HEAD
 import keepApp from './apps/miss-keep/keep-app.cmp.js';
-import mailApp from './pages/mail-app.cmp.js';
-=======
-import keepApp from './pages/keep-app.cmp.js';
 import mailApp from './apps/mister-mail/pages/mail-app.cmp.js';
->>>>>>> 693f2999fad8e8e07fa84dace75d686c03b214fc
 import bookApp from './pages/book-app.cmp.js';
 
 const routes = [

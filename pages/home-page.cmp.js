7f656abee8de476553a3abe0529560e53cef49d3
Bulla Lulla <bulla@YBKof.local>
2020-11-08 01:03:38 +0200

    home page photo fixed

--- pages/home-page.cmp.js
+++ pages/home-page.cmp.js
@@ -4,7 +4,7 @@ export default {
     <section class="home-page flex-column">
     <h1>Welcome to Appsuse!</h1>
         <h3>A super duper app!</h3>
-        <img src="/assets/home-page-img.jpeg">
+        <img src="assets/home-page-img.jpeg">
         <h4>Karin Bitan & Gal Ovadia</h4>
     </section>
     `,
